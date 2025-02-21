e02685180458ef7c609126d65cc858a321689a4c
Mostafa <mostafa_haidare@yahoo.com>
2025-02-21 13:23:25 +0100

    music assignment

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("cd-form");
+const list = document.getElementById("cd-list");
+const template = document.getElementById ("cd-template");
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+
+    const author = document.getElementById("author").value;
+    const title = document.getElementById("title").value;
+    const year = document.getElementById("year").value;
+
+    const newRow = template.content.cloneNode(true);
+    newRow.querySelector (".cd-author").textContent = author;
+    newRow.querySelector (".cd-title"). textContent = title;
+    newRow.querySelector (".cd-year").textContent = year;
+
+
+    newRow.querySelector(".delete-cd").addEventListener ("click", (e)=> {
+        e.target.closest  ("tr").remove();
+    })
+
+    list.appendChild(newRow);
+    form.reset();
+})
+
+
